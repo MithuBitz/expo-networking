bfe0a148968f2aea8436346f6a7e4645023c2083
Mithu Bortamuly <30743120+MithuBitz@users.noreply.github.com>
2026-05-23 23:33:49 +0530

    implement expo api routes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -11,7 +11,7 @@ export default function Index() {
           "https://api.freeapi.app/api/v1/public/randomusers?page=1&limit=10",
         );
         const data = await res.json();
-        console.log(data.data);
+        // console.log(data.data);
 
         setData(data);
       } catch (error) {
