7377eab2f381b8b6be60d3e2f2fd5eaf6fc87485
Mithu Bortamuly <30743120+MithuBitz@users.noreply.github.com>
2026-05-23 23:09:56 +0530

    Implement fetch on expo app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,26 @@
-import { Text, View, StyleSheet } from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    async function fetchUserData() {
+      try {
+        const res = await fetch(
+          "https://api.freeapi.app/api/v1/public/randomusers?page=1&limit=10",
+        );
+        const data = await res.json();
+        console.log(data.data);
+
+        setData(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchUserData();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Edit src/app/index.tsx to edit this screen.</Text>
